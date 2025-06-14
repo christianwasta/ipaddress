0d9486581327b629f194b4311b3d4c4075b66474
wasta <ccw5294@psu.edu>
2025-06-15 02:48:36 +0800

    host pc ip via env variable

--- import ipaddress.py
+++ import ipaddress.py
@@ -1,8 +1,12 @@
 import socket
 import requests
+import os
 
 host_ip = socket.gethostbyname(socket.gethostname())
 print(f"Host IP: {host_ip}")
 
 ip = requests.get("https://api.ipify.org").text
 print(f"Public IP address: {ip}")
+
+host_ip = os.getenv("HOST_IP", "Not provided")
+print(f"Host IP_v4: {host_ip}")
